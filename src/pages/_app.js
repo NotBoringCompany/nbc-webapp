b1f91fde36b33acec651ecb95a213c04b93f34cd
suwandre <suwandresukijat@gmail.com>
2023-02-13 23:49:40 +0100

    started implementing navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,16 @@
-import '@/styles/globals.css';
+// import '@/styles/globals.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import CustomFonts from '@/components/fonts/CustomFonts';
+import { MoralisProvider } from 'react-moralis';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Page title</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-
+    <MoralisProvider
+      appId={`${process.env.NEXT_PUBLIC_MORALIS_APPID}`}
+      serverUrl={`${process.env.NEXT_PUBLIC_MORALIS_SERVERURL}`}
+    >
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
@@ -24,6 +23,6 @@ export default function App({ Component, pageProps }) {
         <CustomFonts />
         <Component {...pageProps} />
       </MantineProvider>
-    </>
+    </MoralisProvider>
   );
 }
