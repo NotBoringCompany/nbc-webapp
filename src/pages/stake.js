4c980d67dbd1bc171f8f1fc46869fdaf9ec18d7a
shonsirsha <seangeekpro@gmail.com>
2023-04-21 16:22:03 +0200

    feat: created stake page

---
+++ src/pages/stake.js
@@ -0,0 +1,18 @@
+import NavbarMenu from "@/components/Dropdowns/NavbarMenu";
+import MainNavbar from "@/components/Navbar/Navbar";
+
+export default function Stake() {
+    return (
+        <div>
+            asd
+        </div>
+    );
+}
+
+export async function getServerSideProps(ctx){
+    return {
+        props:{
+            data:null
+        }
+    }
+}
