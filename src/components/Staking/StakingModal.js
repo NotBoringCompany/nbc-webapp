e46aba79e7bfdf849eba658691cee4054db5452b
suwandre <suwandresukijat@gmail.com>
2023-04-27 10:15:10 +0200

    replaced double quotes with single

--- src/components/Staking/StakingModal.js
+++ src/components/Staking/StakingModal.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
 	Modal,
 	SimpleGrid,
@@ -8,10 +8,10 @@ import {
 	Box,
 	Collapse,
 	Loader,
-} from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import NFTCardPreview from "./NFTCardPreview";
-import { useRouter } from "next/router";
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import NFTCardPreview from './NFTCardPreview';
+import { useRouter } from 'next/router';
 
 const StakingModal = ({
 	showStakingModal,
@@ -20,9 +20,9 @@ const StakingModal = ({
 	loadingStakingRewardAndPoints,
 }) => {
 	const cardColumnsBreakpoints = [
-		{ maxWidth: "xl", cols: 3, spacing: "md" },
-		{ minWidth: "md", cols: 3, spacing: "md" },
-		{ maxWidth: "xs", cols: 3, spacing: "md" },
+		{ maxWidth: 'xl', cols: 3, spacing: 'md' },
+		{ minWidth: 'md', cols: 3, spacing: 'md' },
+		{ maxWidth: 'xs', cols: 3, spacing: 'md' },
 	];
 
 	const { reload, push } = useRouter();
@@ -45,29 +45,29 @@ const StakingModal = ({
 	const stakingPreview = (
 		<>
 			{loadingStakingRewardAndPoints ? (
-				<Loader color="green" />
+				<Loader color='green' />
 			) : (
 				<>
 					<Text
-						size="md"
-						mb="md"
+						size='md'
+						mb='md'
 						sx={{
 							strong: {
-								color: "#42ca9f",
+								color: '#42ca9f',
 							},
 						}}
 					>
-						This staking pool has a total reward of:{" "}
+						This staking pool has a total reward of:{' '}
 						<strong>500000 REC tokens.</strong>
-						<br /> Total staking pool points generated so far:{" "}
+						<br /> Total staking pool points generated so far:{' '}
 						<strong>123 points.</strong> <br />
-						With your current staking combo, you will earn{" "}
+						With your current staking combo, you will earn{' '}
 						<strong>1212 points</strong> for this subpool.
 						<br />
-						As of now, you will be eligible to earn{" "}
-						<strong>5512 tokens.*</strong>{" "}
+						As of now, you will be eligible to earn{' '}
+						<strong>5512 tokens.*</strong>{' '}
 					</Text>
-					<Text size="sm" my="md" color="#42ca9f">
+					<Text size='sm' my='md' color='#42ca9f'>
 						<i>
 							*please bear in mind that this number will decrease as more users
 							stake.
@@ -79,26 +79,26 @@ const StakingModal = ({
 			<Box>
 				<Text
 					sx={{
-						textDecoration: "underline",
-						":hover": {
-							cursor: "pointer",
+						textDecoration: 'underline',
+						':hover': {
+							cursor: 'pointer',
 						},
 					}}
 					onClick={toggle}
 				>
-					{opened ? "Hide" : "See"} your selected keys combination
+					{opened ? 'Hide' : 'See'} your selected keys combination
 				</Text>
 
 				<Collapse in={opened}>
 					<Flex
 						sx={{
-							border: "2px solid #42ca9f",
-							maxHeight: "200px",
-							overflowY: "auto",
+							border: '2px solid #42ca9f',
+							maxHeight: '200px',
+							overflowY: 'auto',
 						}}
-						p={"sm"}
-						mt="md"
-						direction="column"
+						p={'sm'}
+						mt='md'
+						direction='column'
 					>
 						<SimpleGrid breakpoints={cardColumnsBreakpoints}>
 							{subpool.keys.map((key) => (
@@ -112,19 +112,19 @@ const StakingModal = ({
 					</Flex>
 				</Collapse>
 				<Button
-					w="160px"
-					h="50px"
-					mt="md"
-					radius="md"
-					variant="light"
-					color="green"
+					w='160px'
+					h='50px'
+					mt='md'
+					radius='md'
+					variant='light'
+					color='green'
 					onClick={handleStakingButtonClick}
 					disabled={loadingStakingRewardAndPoints || loadingStaking}
 				>
 					{loadingStaking ? (
-						<Loader color="grey" />
+						<Loader color='grey' />
 					) : (
-						<Text size={"lg"}>Stake!</Text>
+						<Text size={'lg'}>Stake!</Text>
 					)}
 				</Button>
 			</Box>
@@ -132,39 +132,39 @@ const StakingModal = ({
 	);
 
 	const successContent = (
-		<Flex direction={"column"}>
-			<Text size="lg" my="md" color="#42ca9f">
+		<Flex direction={'column'}>
+			<Text size='lg' my='md' color='#42ca9f'>
 				Congratulations, staking was <strong>successful!</strong>
 			</Text>
 
-			<Flex mt="md">
+			<Flex mt='md'>
 				<Button
-					w="200px"
-					h="50px"
-					mr="md"
-					radius="md"
-					variant="outline"
+					w='200px'
+					h='50px'
+					mr='md'
+					radius='md'
+					variant='outline'
 					sx={(theme) => ({
 						color: theme.colors.nbcGreen[0],
 						borderColor: theme.colors.nbcGreen[0],
 					})}
 					onClick={reload}
 				>
-					<Text size={"sm"}>Stake again</Text>
+					<Text size={'sm'}>Stake again</Text>
 				</Button>
 
 				<Button
-					w="200px"
-					h="50px"
-					radius="md"
-					variant="outline"
+					w='200px'
+					h='50px'
+					radius='md'
+					variant='outline'
 					sx={(theme) => ({
 						color: theme.colors.nbcGreen[0],
 						borderColor: theme.colors.nbcGreen[0],
 					})}
-					onClick={() => push("/my-subpools")}
+					onClick={() => push('/my-subpools')}
 				>
-					<Text size={"sm"}>See your subpools</Text>
+					<Text size={'sm'}>See your subpools</Text>
 				</Button>
 			</Flex>
 		</Flex>
@@ -175,14 +175,14 @@ const StakingModal = ({
 			opened={showStakingModal}
 			centered
 			onClose={onCloseStakingModal}
-			size={"xl"}
+			size={'xl'}
 			closeOnEscape={!successfulStake}
 			withCloseButton={!successfulStake}
 			closeOnClickOutside={!successfulStake}
 			title={
 				successfulStake
-					? "Successful Staking"
-					: "Preview of Your Staking Subpool"
+					? 'Successful Staking'
+					: 'Preview of Your Staking Subpool'
 			}
 		>
 			{successfulStake ? successContent : stakingPreview}
