462d8e505066d4c443ad64b70a3fdf7c74b93ac4
shonsirsha <seangeekpro@gmail.com>
2023-04-28 16:31:14 +0200

    cleanup index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,4 @@
 import Layout from '@/components/Layout/Layout';
-import { useEffect } from 'react';
-import { useMoralis } from 'react-moralis';
-
 export default function Home() {
-  const { isAuthenticated, user, login, setUserData, isAuthenticating } =
-    useMoralis();
-
-  return (
-    <Layout>
-      <button
-        onClick={async () => {
-          await setUserData({ email: 'kaktus@mail.com', password: '123456' });
-        }}
-      >
-        set data
-      </button>
-
-      <button
-        onClick={async () => {
-          const x = await login('xxx@mail.com', '1234');
-          console.log({ x });
-        }}
-      >
-        login
-      </button>
-    </Layout>
-  );
+  return <Layout></Layout>;
 }
