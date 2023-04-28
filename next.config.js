c59d38759899d877487fce15572adf98c113cf92
shonsirsha <seangeekpro@gmail.com>
2023-04-28 12:42:03 +0200

    installed next-secure-headers

--- next.config.js
+++ next.config.js
@@ -1,38 +1,39 @@
-const withFonts = require('next-fonts');
-const withVideos = require('next-videos');
+const withFonts = require("next-fonts");
+const withVideos = require("next-videos");
+const { createSecureHeaders } = require("next-secure-headers");
 
-module.exports = withVideos(withFonts({
-  webpack(config, options) {
-    return config;
-  },
-  async headers() {
-		return [
-			{
-				source: "/(.*)",
-				headers: createSecureHeaders({
-					contentSecurityPolicy: {
-						directives: {
-							frameAncestors: [
-								"self",
-							],
-							styleSrc: ["'self'", "'unsafe-inline'"],
-							imgSrc: [
-								"'self'",
-                "realmhunter-kos.fra1.cdn.digitaloceanspaces.com",
-							],
-							baseUri: "self",
+module.exports = withVideos(
+	withFonts({
+		webpack(config, options) {
+			return config;
+		},
+		async headers() {
+			return [
+				{
+					source: "/(.*)",
+					headers: createSecureHeaders({
+						contentSecurityPolicy: {
+							directives: {
+								frameAncestors: ["self"],
+								styleSrc: ["'self'", "'unsafe-inline'"],
+								imgSrc: [
+									"'self'",
+									"realmhunter-kos.fra1.cdn.digitaloceanspaces.com",
+								],
+								baseUri: "self",
+							},
 						},
-					},
-					forceHTTPSRedirect: [
-						true,
-						{ maxAge: 63072000, includeSubDomains: true },
-					],
-					noopen: "noopen",
-					nosniff: "nosniff",
-					xssProtection: "sanitize",
-					referrerPolicy: "strict-origin-when-cross-origin",
-				}),
-			},
-		];
-	},
-}));
+						forceHTTPSRedirect: [
+							true,
+							{ maxAge: 63072000, includeSubDomains: true },
+						],
+						noopen: "noopen",
+						nosniff: "nosniff",
+						xssProtection: "sanitize",
+						referrerPolicy: "strict-origin-when-cross-origin",
+					}),
+				},
+			];
+		},
+	})
+);
