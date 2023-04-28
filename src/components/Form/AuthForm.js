68f3e19d2849f0f2f559a0b4e9015469b08298c7
shonsirsha <seangeekpro@gmail.com>
2023-04-28 16:33:43 +0200

    modify AuthForm to be rendered in 2 days (see component for more details)

--- src/components/Form/AuthForm.js
+++ src/components/Form/AuthForm.js
@@ -2,9 +2,15 @@ import { useForm } from '@mantine/form';
 import { useMoralis } from 'react-moralis';
 import { Text, TextInput, Button, Loader } from '@mantine/core';
 
+/**
+ * `AuthForm` renders in 2 different "styles":
+ * 1. When user has email and password already set / linked to their wallet - it'll show as "Change email & password"
+ * 2. When user has neither email and password set / linked to their wallet, it'll show as "Set / link email & password"
+ *
+ */
 const AuthForm = () => {
-  const { isAuthenticated, user, login, setUserData, isUserUpdating } =
-    useMoralis();
+  const { isAuthenticated, user, setUserData, isUserUpdating } = useMoralis();
+  const hasEmail = user && user.attributes.email;
 
   const form = useForm({
     initialValues: { email: '', password: '' },
@@ -26,64 +32,94 @@ const AuthForm = () => {
       await setUserData({ email, password });
     }
   };
-  return (
-    <form onSubmit={form.onSubmit(handleFormSubmit)}>
-      <TextInput
-        my="sm"
-        sx={{
-          input: {
-            marginTop: '4px',
-            padding: '24px 16px',
-            border: '#42ca9f 2px solid',
-            ':focus': {
-              border: '2px solid #42ca9f',
-            },
-          },
-        }}
-        label="Email"
-        type="email"
-        placeholder="Email"
-        {...form.getInputProps('email')}
-      />
-      <TextInput
-        label="Password"
-        sx={{
-          input: {
-            marginTop: '4px',
-            padding: '24px 16px',
-            border: '#42ca9f 2px solid',
-            ':focus': {
-              border: '2px solid #42ca9f',
-            },
-          },
-        }}
-        type="password"
-        placeholder="Password"
-        {...form.getInputProps('password')}
-      />
 
-      <Button
-        h="48px"
-        miw="200px"
-        disabled={isUserUpdating}
-        type="submit"
-        mt="lg"
-        sx={{
-          background: '#42ca9f',
-          transitionDuration: '200ms',
-          ':hover': {
-            transform: 'scale(1.01) translate(1px, -3px)',
-            backgroundColor: '#42ca9f',
+  const hasEmailLeadText = <>Change your email and password</>;
+  const doesntHaveEmailLeadText = (
+    <>
+      It seems like you haven{`'`}t connected your email yet. <br />
+      <span>Connect your email and password</span> to easily log in next time.
+    </>
+  );
+
+  return (
+    <>
+      <Text
+        sx={(theme) => ({
+          span: {
+            color: theme.colors.nbcGreen[0],
+            fontWeight: 500,
           },
-        }}
+        })}
+        size={'md'}
       >
-        {isUserUpdating ? (
-          <Loader color="green" />
-        ) : (
-          <Text size="md">Set Email & Password</Text>
-        )}
-      </Button>
-    </form>
+        {!hasEmail ? doesntHaveEmailLeadText : hasEmailLeadText}
+      </Text>
+      <form sx onSubmit={form.onSubmit(handleFormSubmit)}>
+        <TextInput
+          my="sm"
+          sx={(theme) => ({
+            input: {
+              maxWidth: '400px',
+              width: '100%',
+              marginTop: '4px',
+              padding: '24px 16px',
+              border: `${theme.colors.nbcGreen[0]} 2px solid`,
+              borderRadius: '8px',
+              ':focus': {
+                border: '2px solid #42ca9f',
+              },
+            },
+          })}
+          label="Email"
+          type="email"
+          placeholder="Email"
+          {...form.getInputProps('email')}
+        />
+        <TextInput
+          label="Password"
+          sx={(theme) => ({
+            input: {
+              maxWidth: '400px',
+              width: '100%',
+              marginTop: '4px',
+              padding: '24px 16px',
+              border: `${theme.colors.nbcGreen[0]} 2px solid`,
+              borderRadius: '8px',
+              ':focus': {
+                border: '2px solid #42ca9f',
+              },
+            },
+          })}
+          type="password"
+          placeholder="Password"
+          {...form.getInputProps('password')}
+        />
+
+        <Button
+          h="48px"
+          miw="200px"
+          disabled={isUserUpdating}
+          type="submit"
+          mt="lg"
+          sx={{
+            background: '#42ca9f',
+            transitionDuration: '200ms',
+            ':hover': {
+              transform: 'scale(1.01) translate(1px, -3px)',
+              backgroundColor: '#42ca9f',
+            },
+          }}
+        >
+          {isUserUpdating ? (
+            <Loader color="green" />
+          ) : (
+            <Text size="md">
+              {hasEmail ? `Change` : `Set`} Email & Password
+            </Text>
+          )}
+        </Button>
+      </form>
+    </>
   );
 };
 
