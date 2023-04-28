83dbd4c357ff0b729e81e05f05ff5ab6a6716803
suwandre <suwandresukijat@gmail.com>
2023-04-28 11:37:30 +0200

    implemented security headers on next.config.js

--- next.config.js
+++ next.config.js
@@ -4,5 +4,35 @@ const withVideos = require('next-videos');
 module.exports = withVideos(withFonts({
   webpack(config, options) {
     return config;
-  }
+  },
+  async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: createSecureHeaders({
+					contentSecurityPolicy: {
+						directives: {
+							frameAncestors: [
+								"self",
+							],
+							styleSrc: ["'self'", "'unsafe-inline'"],
+							imgSrc: [
+								"'self'",
+                "realmhunter-kos.fra1.cdn.digitaloceanspaces.com",
+							],
+							baseUri: "self",
+						},
+					},
+					forceHTTPSRedirect: [
+						true,
+						{ maxAge: 63072000, includeSubDomains: true },
+					],
+					noopen: "noopen",
+					nosniff: "nosniff",
+					xssProtection: "sanitize",
+					referrerPolicy: "strict-origin-when-cross-origin",
+				}),
+			},
+		];
+	},
 }));
