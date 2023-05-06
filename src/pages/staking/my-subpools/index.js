bcfbdeb7a6f82be180e87053c8b0dd3617c9dcae
shonsirsha <seangeekpro@gmail.com>
2023-05-06 18:19:20 +0200

    fixed infinite API call error and correcting justify content for subpool list

--- src/pages/staking/my-subpools/index.js
+++ src/pages/staking/my-subpools/index.js
@@ -8,10 +8,9 @@ import {
   Text,
   Tooltip,
 } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
 import { IconAlertOctagon } from '@tabler/icons';
 import { useRouter } from 'next/router';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useCallback, useEffect, useState } from 'react';
 import { useMoralis } from 'react-moralis';
 
 const MySubpools = () => {
@@ -19,7 +18,7 @@ const MySubpools = () => {
   const router = useRouter();
   const [stakerSubpools, setStakerSubpools] = useState(null);
 
-  const fetchStakerSubpools = async () => {
+  const fetchStakerSubpools = useCallback(async () => {
     const subpoolRawRes = await fetch(
       `https://nbc-webapp-api-production.up.railway.app/kos/get-staker-subpools/${
         user && user.attributes.ethAddress
@@ -28,11 +27,13 @@ const MySubpools = () => {
     const subpoolRes = await subpoolRawRes.json();
 
     setStakerSubpools(subpoolRes?.data?.stakerSubpools);
-  };
+  }, [user]);
 
   useEffect(() => {
-    fetchStakerSubpools();
-  }, [user, user?.attributes]);
+    if (user && user.attributes.ethAddress) {
+      fetchStakerSubpools();
+    }
+  }, [fetchStakerSubpools, user, user?.attributes.ethAddress]);
 
   return (
     <Layout
@@ -68,16 +69,13 @@ const MySubpools = () => {
           <Text sx={{ marginTop: 25, marginBottom: 5 }} size={24} weight={600}>
             SUBPOOL LIST
           </Text>
-          <Flex justify='center'>
-            <Divider
-              color='#42ca9f'
-              style={{
-                width: '20%',
-                marginLeft: '40%',
-                marginRight: '40%',
-              }}
-            />
-          </Flex>
+          <Divider
+            color='#42ca9f'
+            style={{
+              width: '20%',
+              margin: '0 auto',
+            }}
+          />
           {!stakerSubpools && (
             <Text size={20} style={{ marginTop: 30, marginBottom: 30 }}>
               You don{"'"}t have any subpools yet. Go to the staking page and
@@ -90,6 +88,7 @@ const MySubpools = () => {
                 <Flex
                   direction='row'
                   align='center'
+                  justify={'space-between'}
                   sx={(theme) => ({
                     padding: '20px 20px',
                   })}
