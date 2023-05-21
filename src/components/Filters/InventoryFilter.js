e688c0b6cf35b788142d440dd7c1521b7126c07f
suwandre <suwandresukijat@gmail.com>
2023-05-21 13:20:29 +0200

    added state changes for filters

--- src/components/Filters/InventoryFilter.js
+++ src/components/Filters/InventoryFilter.js
@@ -21,13 +21,32 @@ const { default: BorderedBox } = require('../BorderedBox/BorderedBox');
 
 const InventoryFilter = () => {
     const [openHouse, { toggle: toggleOpenHouse }] = useDisclosure(false);
-    const [houses, setHouses] = useState([]); // ['tradition', 'chaos', 'glory']
+    const [houses, setHouses] = useState([]);
+    const [types, setTypes] = useState([]);
     const [openType, { toggle: toggleOpenType }] = useDisclosure(false);
     const [luckRating, setLuckRating] = useState(100);
     const [endLuckRating, setEndLuckRating] = useState(100);
     const [luckBoost, setLuckBoost] = useState(100);
     const [endLuckBoost, setEndLuckBoost] = useState(100);
 
+    const handleHouseChange = (e) => {
+        const { checked, id } = e.target;
+        if (checked) {
+            setHouses((prev) => [...prev, id]);
+        } else {
+            setHouses((prev) => prev.filter((house) => house !== id));
+        }
+    }
+
+    const handleTypeChange = (e) => {
+        const { checked, id } = e.target;
+        if (checked) {
+            setTypes((prev) => [...prev, id]);
+        } else {
+            setTypes((prev) => prev.filter((house) => house !== id));
+        }
+    }
+    
     return (
         <BorderedBox
             sx={{ marginTop: 25, padding: '10px 20px 10px 20px' }}
@@ -55,9 +74,9 @@ const InventoryFilter = () => {
             <Collapse in={openHouse}>
                 <Flex direction='column' mb={20}>
                     <Group mb='xs'>
-                        <Checkbox label='Tradition' />
-                        <Checkbox label='Chaos' />
-                        <Checkbox label='Glory' />
+                        <Checkbox label='Tradition' id='Tradition' onClick={(e) => handleHouseChange(e)} />
+                        <Checkbox label='Chaos' id='Chaos' onClick={(e) => handleHouseChange(e)} />
+                        <Checkbox label='Glory' id='Glory' onClick={(e) => handleHouseChange(e)} />
                     </Group>
                 </Flex>
             </Collapse>
@@ -74,29 +93,29 @@ const InventoryFilter = () => {
             <Collapse in={openType}>
                 <Flex mb={20} direction='column'>
                     <Group mb='xs' grow>
-                        <Checkbox label='Brawler' />
-                        <Checkbox label='Crystal' />
-                        <Checkbox label='Earth' />
+                        <Checkbox label='Brawler' id='Brawler' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Crystal' id='Crystal' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Earth' id='Earth' onClick={(e) => handleTypeChange(e)} />
                     </Group>
                     <Group mb='xs' grow>
-                        <Checkbox label='Electric' />
-                        <Checkbox label='Fire' />
-                        <Checkbox label='Frost' />
+                        <Checkbox label='Electric' id='Electric' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Fire' id='Fire' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Frost' id='Frost' onClick={(e) => handleTypeChange(e)} />
                     </Group>
                     <Group mb='xs' grow>
-                        <Checkbox label='Magic' />
-                        <Checkbox label='Nature' />
-                        <Checkbox label='Ordinary' />
+                        <Checkbox label='Magic' id='Magic' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Nature' id='Nature' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Ordinary' id='Ordinary' onClick={(e) => handleTypeChange(e)} />
                     </Group>
                     <Group mb='xs' grow>
-                        <Checkbox label='Psychic' />
-                        <Checkbox label='Reptile' />
-                        <Checkbox label='Spirit' />
+                        <Checkbox label='Psychic' id='Psychic' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Reptile' id='Reptile' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Spirit' id='Spirit' onClick={(e) => handleTypeChange(e)} />
                     </Group>
                     <Group mb='xs' grow>
-                        <Checkbox label='Toxic' />
-                        <Checkbox label='Water' />
-                        <Checkbox label='Wind' />
+                        <Checkbox label='Toxic' id='Toxic' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Water' id='Water' onClick={(e) => handleTypeChange(e)} />
+                        <Checkbox label='Wind' id='Wind' onClick={(e) => handleTypeChange(e)} />
                     </Group>
                 </Flex>
             </Collapse>
